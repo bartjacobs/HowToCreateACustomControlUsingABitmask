db01c614915275c09d0802009ce557fee4953ec4
Bart Jacobs <developer@bartjacobs.com>
2017-05-02 14:47:03 +0200

    Integrate SchedulePicker into ViewController

--- Schedules/ViewController.swift
+++ Schedules/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+
+    @IBOutlet var schedulePicker: SchedulePicker!
+
+    // MARK: - View Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Setup View
+        setupView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - View Methods
+
+    fileprivate func setupView() {
+        // Setup Schedule Picker
+        setupSchedulePicker()
     }
 
+    // MARK: -
+
+    fileprivate func setupSchedulePicker() {
+        // Fetch Stored Value
+        let scheduleRawValue = UserDefaults.standard.integer(forKey: UserDefaults.Keys.schedule)
+
+        // Configure Schedule Picker
+        schedulePicker.schedule = Schedule(rawValue: scheduleRawValue)
+    }
+
+    // MARK: - Actions
+
+    @IBAction func scheduleDidChange(_ sender: SchedulePicker) {
+        // Helpers
+        let userDefaults = UserDefaults.standard
+
+        // Store Value
+        let scheduleRawValue = sender.schedule.rawValue
+        userDefaults.set(scheduleRawValue, forKey: UserDefaults.Keys.schedule)
+        userDefaults.synchronize()
+    }
 
 }
 
+extension UserDefaults {
+
+    enum Keys {
+        
+        static let schedule = "schedule"
+        
+    }
+    
+}
